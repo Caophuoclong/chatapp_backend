61198d351ccf7f4939cd8bbf79c292d699df632d
phuoclong <caophuoclong1@gmail.com>
2022-08-16 21:43:02 +0700

    chore: uipdate

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,11 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { GetUserDto } from './dto/get-user-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   register(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    let user = new User();
+    user = { ...createUserDto };
+    return 'Create user successfully!';
   }
 
   login(getUserDto: GetUserDto) {
